07b955c112b3b316c3dbdefb5f7e742ff4796887
Jess <jess@me3d.com.au>
2020-07-26 19:39:42 +1000

    Add some more APIs to the editor

--- libs/core/sims.d.ts
+++ libs/core/sims.d.ts
@@ -1,18 +1,35 @@
 // Auto-generated from simulator. Do not edit.
 declare namespace shapes {
+    /**
+     * Add a cube
+     * @param width The width of the cube
+     * @param depth The depth of the cube
+     * @param height The height of the cube
+     */
     //% block="cube width $width|depth $depth|height $height"
     //% inlineInputMode=inline
     //% width.defl=10
     //% depth.defl=10
     //% height.defl=10
+    //% weight=10
+    //% group="3D Shapes"
+    //% expandableArgumentMode="enabled"
     //% shim=shapes::cube
     function cube(width: number, depth: number, height: number): void;
 
+    /**
+     * Add a sphere
+     * @param width The width of the sphere
+     * @param depth The depth of the sphere
+     * @param height The height of the sphere
+     */
     //% block="sphere width $width|depth $depth|height $height"
     //% inlineInputMode=inline
-    //% width.defl=10
-    //% depth.defl=10
-    //% height.defl=10
+    //% width.defl=50
+    //% depth.defl=50
+    //% height.defl=50
+    //% weight=20
+    //% group="3D Shapes"
     //% shim=shapes::sphere
     function sphere(width: number, depth: number, height: number): void;
 
@@ -20,13 +37,17 @@ declare namespace shapes {
     //% inlineInputMode=inline
     //% radius.defl=10
     //% height.defl=10
+    //% weight=30
+    //% group="3D Shapes"
     //% shim=shapes::cylinder
     function cylinder(radius: number, height: number): void;
 
     //% block="donut thickness $thickness|radius $radius"
     //% inlineInputMode=inline
-    //% thickess.defl=20
+    //% thickness.defl=20
     //% radius.defl=100
+    //% weight=40
+    //% group="3D Shapes"
     //% shim=shapes::donut
     function donut(thickness: number, radius: number): void;
 
@@ -34,40 +55,135 @@ declare namespace shapes {
     //% inlineInputMode=inline
     //% radius.defl=10
     //% height.defl=10
+    //% weight=50
+    //% group="3D Shapes"
     //% shim=shapes::cone
     function cone(radius: number, height: number): void;
 
+    //% block="circle radius $radius"
+    //% inlineInputMode=inline
+    //% radius.defl=10
+    //% weight=50
+    //% group="2D Shapes"
+    //% shim=shapes::circle
+    function circle(radius: number): void;
+
+    //% blockId=add_rect block="rect width $width|height $height"
+    //% inlineInputMode=inline
+    //% width.defl=50
+    //% height.defl=50
+    //% weight=50
+    //% group="2D Shapes"
+    //% shim=shapes::rect
+    function rect(width: number, height: number): void;
+
 }
-declare namespace operators {
+declare namespace position {
+    /**
+     * move shapes across the x axis
+     * @param x how far to move across the x axis
+     * @param body the shapes to move across
+     */
+    //% blockId=move_shapes_across block="move shapes across $x" 
+    //% topblock=false
+    //% x.defl=10
+    //% handlerStatement=true
+    //% group="Position"
+    //% shim=position::moveShapesAcrossAsync promise
+    function moveShapesAcross(x: number, body: () => void): void;
+
+    //% blockId=move_shapes_over block="move shapes over $y" 
+    //% topblock=false
+    //% y.defl=10
+    //% handlerStatement=true
+    //% group="Position"
+    //% shim=position::moveShapesOverAsync promise
+    function moveShapesOver(y: number, body: () => void): void;
+
+    //% blockId=move_shapes_up block="move shapes up $z" 
+    //% topblock=false
+    //% z.defl=10
+    //% handlerStatement=true
+    //% group="Position"
+    //% shim=position::moveShapesUpAsync promise
+    function moveShapesUp(z: number, body: () => void): void;
+
+    //% blockId=move_shapes block="translate shapes x: $x|  y: $y |  z: $z" 
+    //% topblock=false
+    //% handlerStatement=true
+    //% group="Position"
+    //% advanced=true
+    //% shim=position::translateShapesAsync promise
+    function translateShapes(x: number, y: number, z: number, body: () => void): void;
+
+    //% blockId=flip_shapes block="flip shapes $x °" 
+    //% topblock=false
+    //% handlerStatement=true
+    //% x.shadow="protractorPicker"
+    //% group="Rotation"
+    //% shim=position::flipShapesAsync promise
+    function flipShapes(x: number, body: () => void): void;
+
+    //% blockId=roll_shapes block="roll shapes $y °" 
+    //% topblock=false
+    //% handlerStatement=true
+    //% y.shadow="protractorPicker"
+    //% group="Rotation"
+    //% shim=position::rollShapesAsync promise
+    function rollShapes(y: number, body: () => void): void;
+
+    //% blockId=spin_shapes block="roll shapes $z °" 
+    //% topblock=false
+    //% handlerStatement=true
+    //% z.shadow="protractorPicker"
+    //% group="Rotation"
+    //% shim=position::spinShapesAsync promise
+    function spinShapes(z: number, body: () => void): void;
+
+    //% blockId=rotate_shapes block="rotate shapes x°: $x|  y°: $y | z°: $z" 
+    //% topblock=false
+    //% handlerStatement=true
+    //% group="Rotation"
+    //% advanced=true
+    //% shim=position::rotateShapesAsync promise
+    function rotateShapes(x: number, y: number, z: number, body: () => void): void;
+
     //% blockId=add_shapes block="add shapes" 
     //% topblock=false
     //% handlerStatement=true
-    //% shim=operators::addShapes
+    //% group="Operations"
+    //% shim=position::addShapes
     function addShapes(body: () => void): void;
 
     //% blockId=subtract_shapes block="subtract shapes" 
     //% topblock=false
     //% handlerStatement=true
-    //% shim=operators::subtractShapesAsync promise
+    //% group="Operations"
+    //% shim=position::subtractShapesAsync promise
     function subtractShapes(body: () => void): void;
 
     //% blockId=intersect_shapes block="intersect shapes" 
     //% topblock=false
     //% handlerStatement=true
-    //% shim=operators::intersectShapesAsync promise
+    //% group="Operations"
+    //% shim=position::intersectShapesAsync promise
     function intersectShapes(body: () => void): void;
 
-    //% blockId=move_shapes block="move shapes across x: $x| over y: $y | up z: $z" 
+    //% blockId=wrap2dshapes block="wrap 2d shapes (hull)" 
     //% topblock=false
     //% handlerStatement=true
-    //% shim=operators::moveShapesAsync promise
-    function moveShapes(x: number, y: number, z: number, body: () => void): void;
+    //% group="Operations"
+    //% advanced=true
+    //% shim=position::wrap2DShapesAsync promise
+    function wrap2DShapes(body: () => void): void;
 
-    //% blockId=rotate_shapes block="rotate shapes x: $x|  y: $y | z: $z" 
+    //% blockId=sequentialWrap2dshapes block="wrap 2d shapes sequentially (chain hull)" 
     //% topblock=false
     //% handlerStatement=true
-    //% shim=operators::rotateShapesAsync promise
-    function rotateShapes(x: number, y: number, z: number, body: () => void): void;
+    //% group="Operations"
+    //% advanced=true
+    //% shim=position::sequentialWrap2DShapesAsync promise
+    function sequentialWrap2DShapes(body: () => void): void;
 
 }
 
--- sim/api.ts
+++ sim/api.ts
@@ -1,29 +1,51 @@
 /// <reference path="../libs/core/enums.d.ts"/>
 
-
 /**
+ * How to set fields and colours
  * @external https://makecode.com/playground#basic-default-values
+ * @external https://makecode.com/extensions/getting-started/simple-extension
  * use the playground to configure the pickers
  */
 
+ /**
+  * 3D Shapes to create on the surface
+  */
+//% groups='["3D Shapes", "2D Shapes"]'
+//% weight=100 color=#d2b48c icon="\uf1b0"
 namespace pxsim.shapes {
+
+    /**
+     * Add a cube
+     * @param width The width of the cube
+     * @param depth The depth of the cube
+     * @param height The height of the cube
+     */
     //% block="cube width $width|depth $depth|height $height"
     //% inlineInputMode=inline
     //% width.defl=10
     //% depth.defl=10
     //% height.defl=10
+    //% weight=10
+    //% group="3D Shapes"
+    //% expandableArgumentMode="enabled"
     export function cube(width: number = 1, depth: number = 1, height: number = 1) {
         board().addStatement(`cube({size: [${width}, ${depth}, ${height}]})`);
 
     }
 
-
+    /**
+     * Add a sphere
+     * @param width The width of the sphere
+     * @param depth The depth of the sphere
+     * @param height The height of the sphere
+     */
     //% block="sphere width $width|depth $depth|height $height"
     //% inlineInputMode=inline
-    //% width.defl=10
-    //% depth.defl=10
-    //% height.defl=10
-
+    //% width.defl=50
+    //% depth.defl=50
+    //% height.defl=50
+    //% weight=20
+    //% group="3D Shapes"
     export function sphere(width: number, depth: number, height: number) {
         board().addStatement(`sphere({size: [${width}, ${depth}, ${height}]})`);
 
@@ -34,34 +56,171 @@ namespace pxsim.shapes {
     //% inlineInputMode=inline
     //% radius.defl=10
     //% height.defl=10
+    //% weight=30
+     //% group="3D Shapes"
     export function cylinder(radius: number, height: number) {
         board().addStatement(`cylinder({r1: ${radius}, r2: ${radius}, h: ${height}})`);
     }
     //% block="donut thickness $thickness|radius $radius"
     //% inlineInputMode=inline
-    //% thickess.defl=20
+    //% thickness.defl=20
     //% radius.defl=100
+    //% weight=40
+    //% group="3D Shapes"
     export function donut(thickness: number, radius: number) {
         board().addStatement(`torus({ri: ${thickness / 2}, ro: ${radius} })`)
     }
 
+    
     //% block="cone radius $radius|height $height"
     //% inlineInputMode=inline
     //% radius.defl=10
     //% height.defl=10
+    //% weight=50
+    //% group="3D Shapes"
     export function cone(radius: number, height: number) {
         board().addStatement(`cylinder({r2: 0, r1: ${radius}, h: ${height}})`);
     }
 
 
+    //% block="circle radius $radius"
+    //% inlineInputMode=inline
+    //% radius.defl=10
+    //% weight=50
+    //% group="2D Shapes"
+    export function circle(radius: number) {
+        board().addStatement(`circle({r: ${radius}})`);
+    }
+
+    //% blockId=add_rect block="rect width $width|height $height"
+    //% inlineInputMode=inline
+    //% width.defl=50
+    //% height.defl=50
+    //% weight=50
+    //% group="2D Shapes"
+    export function rect(width: number, height: number) {
+        board().addStatement(`square({size: [${width}, ${height}]})`);
+    }
+
+    
+
 
 }
 
-namespace pxsim.operators {
+/**
+ * Move and rotate shapes
+ */
+//% groups=["Position", "Rotation", "Operations"]
+//% color=#4c96f7 weight=21 icon="\uf13e"
+namespace pxsim.position {
 
-    //% blockId=add_shapes block="add shapes" 
+    /**
+     * move shapes across the x axis
+     * @param x how far to move across the x axis
+     * @param body the shapes to move across
+     */
+    //% blockId=move_shapes_across block="move shapes across $x" 
+    //% topblock=false
+    //% x.defl=10
+    //% handlerStatement=true
+    //% group="Position"
+    export function moveShapesAcrossAsync(x: number, body: RefAction): Promise<void> {
+
+        board().addBlock(`translate([${x}, 0, 0], <CHILDREN> )`);
+       
+        return pxsim.runtime.runFiberAsync(body)
+    }
+    
+    //% blockId=move_shapes_over block="move shapes over $y" 
+    //% topblock=false
+    //% y.defl=10
+    //% handlerStatement=true
+    //% group="Position"
+    export function moveShapesOverAsync(y: number, body: RefAction): Promise<void> {
+
+        board().addBlock(`translate([0, ${y}, 0], <CHILDREN> )`);
+       
+        return pxsim.runtime.runFiberAsync(body)
+    }
+
+    //% blockId=move_shapes_up block="move shapes up $z" 
+    //% topblock=false
+    //% z.defl=10
+    //% handlerStatement=true
+    //% group="Position"
+    export function moveShapesUpAsync(z: number, body: RefAction): Promise<void> {
+
+        board().addBlock(`translate([0, 0, ${z}], <CHILDREN> )`);
+       
+        return pxsim.runtime.runFiberAsync(body)
+    }
+ 
+    //% blockId=move_shapes block="translate shapes x: $x|  y: $y |  z: $z" 
     //% topblock=false
     //% handlerStatement=true
+    //% group="Position"
+    //% advanced=true
+    export function translateShapesAsync(x: number, y: number, z: number, body: RefAction): Promise<void> {
+
+        board().addBlock(`translate([${x}, ${y}, ${z}], <CHILDREN> )`);
+       
+        return pxsim.runtime.runFiberAsync(body)
+    }
+
+    //% blockId=flip_shapes block="flip shapes $x °" 
+    //% topblock=false
+    //% handlerStatement=true
+    //% x.shadow="protractorPicker"
+    //% group="Rotation"
+    export function flipShapesAsync(x: number, body: RefAction): Promise<void> {
+
+        board().addBlock(`rotate([${x}, 0, 0], <CHILDREN> )`);
+        return pxsim.runtime.runFiberAsync(body)
+
+    } 
+    //% blockId=roll_shapes block="roll shapes $y °" 
+    //% topblock=false
+    //% handlerStatement=true
+    //% y.shadow="protractorPicker"
+   //% group="Rotation"
+   
+    export function rollShapesAsync(y: number, body: RefAction): Promise<void> {
+
+        board().addBlock(`rotate([0, ${y}, 0], <CHILDREN> )`);
+        return pxsim.runtime.runFiberAsync(body)
+
+    }
+
+    //% blockId=spin_shapes block="roll shapes $z °" 
+    //% topblock=false
+    //% handlerStatement=true
+    //% z.shadow="protractorPicker"
+    //% group="Rotation"
+  
+    export function spinShapesAsync(z: number, body: RefAction): Promise<void> {
+
+        board().addBlock(`rotate([0, 0, ${z}], <CHILDREN> )`);
+        return pxsim.runtime.runFiberAsync(body)
+
+    }
+
+    //% blockId=rotate_shapes block="rotate shapes x°: $x|  y°: $y | z°: $z" 
+    //% topblock=false
+    //% handlerStatement=true
+    //% group="Rotation"
+    //% advanced=true
+    export function rotateShapesAsync(x: number, y: number, z: number, body: RefAction): Promise<void> {
+
+        board().addBlock(`rotate([${x}, ${y}, ${z}], <CHILDREN> )`);
+        return pxsim.runtime.runFiberAsync(body)
+
+    }
+
+
+     //% blockId=add_shapes block="add shapes" 
+    //% topblock=false
+    //% handlerStatement=true
+    //% group="Operations"
     export function addShapes(body: RefAction): void {
         board().addBlock("union( <CHILDREN> )");
 
@@ -71,6 +230,7 @@ namespace pxsim.operators {
     //% blockId=subtract_shapes block="subtract shapes" 
     //% topblock=false
     //% handlerStatement=true
+    //% group="Operations"
     export function subtractShapesAsync(body: RefAction): Promise<void> {
         board().addBlock("difference( <CHILDREN> )"); // add a JSCad statement to the interpreter.
     
@@ -81,6 +241,7 @@ namespace pxsim.operators {
     //% blockId=intersect_shapes block="intersect shapes" 
     //% topblock=false
     //% handlerStatement=true
+    //% group="Operations"
     export function intersectShapesAsync(body: RefAction): Promise<void> {
         board().addBlock("intersect( <CHILDREN> )");
       
@@ -88,27 +249,31 @@ namespace pxsim.operators {
         return pxsim.runtime.runFiberAsync(body)
     
     }
- 
-    //% blockId=move_shapes block="move shapes across x: $x| over y: $y | up z: $z" 
+
+    //% blockId=wrap2dshapes block="wrap 2d shapes (hull)" 
     //% topblock=false
     //% handlerStatement=true
-    export function moveShapesAsync(x: number, y: number, z: number, body: RefAction): Promise<void> {
-
-        board().addBlock(`translate([${x}, ${y}, ${z}], <CHILDREN> )`);
-       
+    //% group="Operations"
+    //% advanced=true
+    export function wrap2DShapesAsync(body: RefAction): Promise<void> {
+        board().addBlock("hull( <CHILDREN> )");
+      
+    
         return pxsim.runtime.runFiberAsync(body)
+    
     }
 
-     //% blockId=rotate_shapes block="rotate shapes x: $x|  y: $y | z: $z" 
+    //% blockId=sequentialWrap2dshapes block="wrap 2d shapes sequentially (chain hull)" 
     //% topblock=false
     //% handlerStatement=true
-    export function rotateShapesAsync(x: number, y: number, z: number, body: RefAction): Promise<void> {
-
-        board().addBlock(`rotate([${x}, ${y}, ${z}], <CHILDREN> )`);
+    //% group="Operations"
+    //% advanced=true
+    export function sequentialWrap2DShapesAsync(body: RefAction): Promise<void> {
+        board().addBlock("chain_hull( <CHILDREN> )");
+      
+    
         return pxsim.runtime.runFiberAsync(body)
-
+    
     }
-
-
 }
 
