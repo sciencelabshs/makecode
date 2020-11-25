323f7373904220aa69c207720e9b2914216e23ff
Jess <jess@me3d.com.au>
2020-11-26 10:26:22 +1100

    Remove unused code

--- sim/public/buildbee/main-index.js
+++ sim/public/buildbee/main-index.js
@@ -5,16 +5,6 @@ us access for certain actions out of the sandbox
 
 const CLIENT_ID = '427e8320137ceca41834'
 
-function stringToArrayBuffer(str) {
-    // https://developers.google.com/web/updates/2012/06/How-to-convert-ArrayBuffer-to-and-from-String
-
-    var buf = new ArrayBuffer(str.length * 2); // 2 bytes for each char
-    var bufView = new Uint8Array(buf);
-    for (var i = 0, strLen = str.length; i < strLen; i++) {
-        bufView[i] = str.charCodeAt(i);
-    }
-    return buf;
-}
 
 function sendToThingiverse(event) {
     if (window.DEBUG_THINGIVERSE) console.log("sendToThingiverse")
